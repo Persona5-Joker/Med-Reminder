a936a0ead90acb9e7927c413c38bae15a378b96b
Danielle Bryan <danidrums1997@gmail.com>
2024-08-31 14:58:20 -0500

    timer.java

--- timer.java
+++ timer.java
@@ -1,1 +1,27 @@
+class timer implements Runnable{
+    private int seconds;
+
+    public timer(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public void run() {
+       for(int i = seconds; i >= 0; i--){
+
+           int hours = i/3600;
+           int minutes = (i*3600)%60;
+           int seconds = i%60;
+
+           String timeFormat = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+           System.out.println(timeFormat);
+
+            try{
+                Thread.sleep(1000);
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+       }
+    }
+}
 
